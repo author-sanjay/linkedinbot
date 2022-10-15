333bd6e85e0f21aff11a2e20701eb2bd9dc4ec97
sanjay <sanjaykumar73189@gmail.com>
2022-10-16 00:19:33 +0530

    got all the links

--- main.py
+++ main.py
@@ -4,15 +4,17 @@ from selenium import webdriver
 from bs4 import BeautifulSoup
 from selenium.webdriver.chrome.service import Service
 
-
-file=open("/home/sanju/PycharmProjects/pythonProject/config.txt")
+baseurl="https://www.linkedin.com"
+passwordfile="/home/sanju/PycharmProjects/pythonProject/config.txt"
+driverlink="/home/sanju/Downloads/chromedriver"
+file=open(passwordfile)
 line=file.readlines()
 my_username=line[0]
 my_password=line[1]
 
 
-website="https://www.linkedin.com/uas/login?fromSignIn=true&trk=cold_join_sign_in"
-service=Service(executable_path='/home/sanju/Downloads/chromedriver')
+website=baseurl+"/uas/login?fromSignIn=true&trk=cold_join_sign_in"
+service=Service(executable_path=driverlink)
 driver=webdriver.Chrome(service=service)
 driver.get(website)
 
@@ -47,12 +49,30 @@ def searchhr(query):
     # service = Service(executable_path='/home/sanju/Downloads/chromedriver')
     # driver = webdriver.Chrome(service=service)
     driver.get(website2)
-
+    # filter only hr
+    hrlist = driver.find_element(by="xpath",value='//*[@id="search-reusables__filters-bar"]/ul/li[1]/button')
+    driver.execute_script("arguments[0].click();", hrlist)
 
 query="tcs%20hr"
 searchhr(query)
 
-#filter only hr
-hrlist=driver.find_element(by="xpath",value='/html/body/div[4]/div[3]/div[2]/section/div/nav/div/ul/li[1]/button')
-driver.execute_script("arguments[0].click();",hrlist)
+visitingID='/in/authorsanju'
+completelink=baseurl+visitingID
 
+visitedProfiles=[]
+profilesQueued=[]
+
+
+time.sleep(5)
+containers=driver.find_elements(by="xpath",value='//*[@id="main"]/div/div/div[1]/ul/li')
+
+for ele in containers:
+
+    link = ele.find_element(by="xpath",value='./div/div/div[2]/div[1]/div[1]/div/span[1]/span/a').get_attribute("href")
+    if link not in visitedProfiles:
+        profilesQueued.append(link)
+
+# print(total)
+# getNewProfileIDs(BeautifulSoup(driver.page_source),profilesQueued)
+
+# print(BeautifulSoup(driver.page_source,"html.parser"))
